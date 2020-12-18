a87154fe46eb025c41fb172c005df7c4749fc46b
Simon Siefke <simon.siefke@gmail.com>
2020-12-18 23:11:55 +0100

    include average

--- forkPtyAndExecvp.benchmark.js
+++ forkPtyAndExecvp.benchmark.js
@@ -5,6 +5,7 @@ const { performance } = require('perf_hooks')
 const terminators = ['$ ', '$ \x1B[0m']
 
 ;(async () => {
+  let total = 0
   for (let i = 0; i < 15; i++) {
     await new Promise((r) => {
       const s = performance.now()
@@ -14,12 +15,14 @@ const terminators = ['$ ', '$ \x1B[0m']
         console.log({ data: data.toString() })
         if (terminators.some((t) => data.toString().endsWith(t))) {
           const e2 = performance.now()
+          total += e2 - s
           console.log({ data: e2 - s })
           r()
         }
       })
     })
   }
+  console.log({ average: total / 15 })
   process.exit(0)
 })()
 
--- nodePty.benchmark.js
+++ nodePty.benchmark.js
@@ -4,20 +4,25 @@ const { performance } = require('perf_hooks')
 const terminators = ['$ ', '$ \x1B[0m']
 
 ;(async () => {
+  let total = 0
   for (let i = 0; i < 15; i++) {
     await new Promise((r) => {
       const s = performance.now()
-      const term = spawn('bash', ['-i'], {})
+      const term = spawn('bash', ['-i'], {
+        env: {},
+      })
       term.onData((data) => {
         console.log({ data: data.toString() })
         if (terminators.some((t) => data.toString().endsWith(t))) {
           const e2 = performance.now()
+          total += e2 - s
           console.log({ data: e2 - s })
           r()
         }
       })
     })
   }
+  console.log({ average: total / 15 })
   process.exit(0)
 })()
 
